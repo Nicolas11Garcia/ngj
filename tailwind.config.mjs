fea206636581c9516c0c633ee37098309e1b8918
Nicolas11Garcia <nicolasgarciajimenez12@gmail.com>
2024-01-22 11:00:50 -0300

    up

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,33 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-const { nextui } = require("@nextui-org/react");
-
 export default {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#242F65",
-        secundary: "#FF774C",
-        negro: "#2A2C32",
-        celeste: "#7390F9",
-        plomo: "#4F4F4F",
-        gray: "#8B8C8C",
-      },
-    },
-  },
-  plugins: [
-    nextui({
-      themes: {
-        light: {
-          // ...
-          colors: {
-            foreground: "#2D2F30",
-          },
-        },
-      },
-    }),
-  ],
-};
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
